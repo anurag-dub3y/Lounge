3f9b24c7b4420f8ea9167bb930702535426f28ed
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2022-07-21 13:10:09 +0530

    First commit

---
+++ controller/userControls.js
@@ -0,0 +1,185 @@
+const User = require('../model/userModel')
+const nodemailer = require("nodemailer");
+const sendToken = require("../utils/jwtToken");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
+
+
+exports.createUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    const token = user.getJWTToken();
+
+    sendToken(user, 201, res);
+}
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // checking if user has given password and email both
+
+    if (!email || !password) {
+        return next(new ErrorHandler("Please enter email & password", 400));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+    // 401 is code for unauthorized
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+
+    sendToken(user, 200, res);
+});
+
+
+// Logout User
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "You have logged Out",
+    });
+});
+
+
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+  
+    res.status(200).json({
+      success: true,
+      user,
+    });
+});
+
+
+// update User password
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select("+password");
+  
+    const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+  
+    if (!isPasswordMatched) {
+      return next(new ErrorHandler("Old password is incorrect", 400));
+    }
+  
+    if (req.body.newPassword !== req.body.confirmPassword) {
+      return next(new ErrorHandler("Both passwords do not match", 400));
+    }
+  
+    user.password = req.body.newPassword;
+  
+    await user.save();
+  
+    sendToken(user, 200, res);
+});
+
+// Delete User --Admin
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not exist with Id: ${req.params.id}`, 400)
+    );
+  }
+
+  await user.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "User Deleted Successfully",
+  });
+});
+
+// FORGOR SECTION
+
+// Forgot Password
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+  
+    if (!user) {
+      return next(new ErrorHandler("User not found", 404));
+    }
+  
+    // Get ResetPassword Token
+    const resetToken = user.getResetPasswordToken();
+  
+    await user.save({ validateBeforeSave: false });
+  
+const resetPasswordUrl = `${req.protocol}://${req.get("host")}/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
+  
+    try {
+      await sendEmail({
+        email: user.email,
+        subject: `Bargain Basement Password Recovery`,
+        message,
+      });
+  
+      res.status(200).json({
+        success: true,
+        message: `Email sent to ${user.email} successfully`,
+      });
+    } catch (error) {
+     console.log(error);
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpire = undefined;
+  
+      await user.save({ validateBeforeSave: false });
+  
+      return next(new ErrorHandler(error.message, 500));
+    }
+  });
+
+// Reset Password, Doesn't work
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+  // creating token hash
+const x = req.body.token;
+const resetPasswordToken = crypto.createHash('sha256').update(x).digest('hex');
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "Reset Password Token is invalid or has been expired",
+        400
+      )
+    );
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password does not match", 400));
+  }
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  sendToken(user, 200, res);
+});
